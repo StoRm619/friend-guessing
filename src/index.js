8dc5cd30dd8246ef3eef167da19d2e9385e4fcb0
Matthews A <matthewsabebe23@gmail.com>
2019-10-27 20:06:14 -0700

    added basic idea in app.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+//Basic idea for code 
+// have each friend have it its own counter
+//if friend counter at any point is more than 2 then good over and reset eveything
+// use following code to increase counter
+//   // handleIncrement increases this.state.count by 1
+//   handleIncrement = () => {
+//     // We always use the setState method to update a component's state
+//     this.setState({ count: this.state.count + 1 });
+//   };
+// move friend card around after each pic.
